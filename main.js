b7d0baf2556f4705cb5b1bfd11ffac5b338f1409
Joshua Hoffman <joshuahoffman2023@u.northwestern.edu>
2020-11-30 13:44:39 -0600

    javascript

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 // Add modal! and/or popup
 function toggleAboutModal() {
     let elem = document.getElementById("about-modal");
-    elem.classList.toggle("show-modal");
+    if (elem) {
+        elem.classList.toggle("show-modal");
+    }
 }
 
 function toggleProjectModal() {
-    console.log("test");
     let elem = document.getElementById("project-modal");
-    elem.classList.toggle("show-modal");
+    if (elem) {
+        elem.classList.toggle("show-modal");
+    }
 }
