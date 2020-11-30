bb65e1a7a7cd6fd6d09eee32e529c4912461b2b0
Joshua Hoffman <joshuahoffman2023@u.northwestern.edu>
2020-11-29 23:55:51 -0600

    modals

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 // Add modal! and/or popup
+function toggleAboutModal() {
+    let elem = document.getElementById("about-modal");
+    elem.classList.toggle("show-modal");
+}
+
+function toggleProjectModal() {
+    console.log("test");
+    let elem = document.getElementById("project-modal");
+    elem.classList.toggle("show-modal");
+}
